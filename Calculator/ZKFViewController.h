932e7ddda8503d5d122cb6b106806e277e0958e6
twer <twer@MobileLab-iMac.local>
2012-06-02 15:52:48 +0800

    first setup

--- Calculator/ZKFViewController.h
+++ Calculator/ZKFViewController.h
@@ -10,4 +10,26 @@
 
 @interface ZKFViewController : UIViewController
 
+@property(nonatomic, retain) UITextField *result;
+
+-(IBAction)clickBtn:(id)sender;
+
+-(IBAction)click1;
+-(IBAction)click2;
+-(IBAction)click3;
+-(IBAction)click4;
+-(IBAction)click5;
+-(IBAction)click6;
+-(IBAction)click7;
+-(IBAction)click8;
+-(IBAction)click9;
+-(IBAction)click0;
+-(IBAction)clickAdd;
+-(IBAction)clickSub;
+-(IBAction)clickMultiply;
+-(IBAction)clickDevide;
+-(IBAction)clickEqual;
+
+
+
 @end
